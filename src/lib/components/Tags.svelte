61df35e7dd049d400da44879b43cb5271749909c
vitor do nascimento <vitornascimento@proton.me>
2025-10-20 22:43:07 -0300

    add component for tagging stuff

---
+++ src/lib/components/Tags.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let items = [];
+</script>
+
+<div class="flex flex-row flex-wrap gap-2">
+	{#each items as item}
+		<span
+			class="text-sm font-bold bg-neutral-700 text-white rounded-xl px-2.5 py-0.5"
+		>
+			{item}
+		</span>
+	{/each}
+</div>
